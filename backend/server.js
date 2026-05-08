125c34ecd7c55590312dd8da35436251a7d39b3e
Nitish <yelagadanitish@gmail.com>
2026-05-08 18:01:15 +0530

    Fixed frontend API routes

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const db = require("./db");
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
